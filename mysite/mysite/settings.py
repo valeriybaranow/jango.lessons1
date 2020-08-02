c27936a166abd238c160c342b2f003057372adcb
Valeriy Baranov <valeriybaranov.job@gmail.com>
2020-08-02 15:22:30 +0300

    Урок 7. Начало создания новостной категории

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'webexample',
     'mainAdd',
+    'news',
 ]
 
 MIDDLEWARE = [
--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -13,12 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.conf.urls import url
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('webexample/', include('webexample.urls')),
-    path('', include('mainAdd.urls')),
-    path('news/', include('news.urls')),
+    url('contact', views.contact, name='contact'),
+    url('', views.index, name='index'),
 ]
